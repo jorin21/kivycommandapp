355a6e17a1e453a1de46f4dae2d95275a5d7b1dc
Jorge Avila <jorgeeavila1@gmail.com>
2022-09-21 16:07:00 -0400

    Created initial downfiles function for the app

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
@@ -12,11 +13,8 @@ import inspect
 
     
 
-
-    
-    
-
-
+global Reply
+Reply = False
 
 #list of commands
 Commands = ['wakepc','sleeppc', 'snap', 'downfiles']
@@ -27,7 +25,7 @@ class CommandHandler():
         self.Command = Command
         self.LineNum = LineNum
         self.Pass = Pass
-
+        self.wait_for_reply = 0
 
     def wakepc(self):
         if self.Command == 'wakepc':
@@ -55,8 +53,23 @@ class CommandHandler():
 
     def sendfiles(self):
         if self.Command == 'downfiles':
-            subjects = 'sendfiles'
-            message = sGen(self.LineNum,self.Pass)
+            self.method_ran = 'sendfiles'
+            if Reply == False:
+                subjects = 'sendfiles'
+                message = sGen(self.LineNum,self.Pass)
+                time.sleep(2)
+                
+                self.wait_for_reply = 1
+                print('wait for reply set 1')
+                sendmail(message,subjects)
+            else:
+                
+                self.Pass = self.Pass.split('\n')
+                
+                subjects = 'Re: DropBox API Call'
+                message = self.Pass[1]
+
+                sendmail(message,subjects)
 
 
             
@@ -74,17 +87,23 @@ class CommandHandler():
 
 
 
-    def run(self): 
+    def run(self):
         methods = inspect.getmembers(CommandHandler, predicate=inspect.isfunction) #gets list of all methods in the class
         for method in methods: #runs each method in the list for the exception of the init run
             if method[0] != '__init__' and method[0] != 'run': 
                 method[1](self)
                 
+                
 
+
+
+    
 
 
 class MyLayout(Widget):
     Commands = Commands
+
+    stop = threading.Event()
 
     def press(self):
         self.Command = self.ids.Commands_Drop.text
@@ -95,28 +114,84 @@ class MyLayout(Widget):
         print(f'{self.Command} , {self.LineNum} , {self.Pass}')
         
         
-        t=threading.Thread(target=self.commandHandleInit, daemon=True)
-        t.start()
+        threading.Thread(target=self.commandHandleInit, daemon=True).start()
         pass
 
     def commandHandleInit(self):
-        cH = CommandHandler(self.Command,self.LineNum,self.Pass)
-        cH.run()
+        self.cH = CommandHandler(self.Command,self.LineNum,self.Pass)
+        self.cH.run()
 
+        if self.cH.wait_for_reply == 1:
+            self.waiting()
+                
+        
+        
 
         # self.ids.Submit_Button.text = "Sent!"
         # time.sleep(2)
         # self.ids.Submit_Button.text = "Submit"
+
+    def waiting(self):
+        while True:
+
+            wait = 'Waiting.'
+            for i in range(3):
+                r = readmail()
+                self.update_but(wait)
+                
+
+
+
+                
+                if self.cH.method_ran == 'sendfiles':
+                    if r['Subject'] == 'Download Files':
+                        self.update_inp(reverse=True)
+                        self.update_but('Submit')
+                        break
+
+                    if r['Subject'] == 'DropBox API Call':
+                        self.update_inp()
+                        self.update_but('Submit')
+                        global Reply
+                        Reply = True
+                        break
+                    else:
+                        print('No API Call yet')
+
+                wait += '.'
+                
+                
+            
+    
+    @mainthread
+    def update_but(self, wait):
+        self.ids.Submit_Button.text = wait
+
+    @mainthread
+    def update_inp(self, reverse=False):
+        if reverse:
+            self.ids.Pass_Input.size = (self.ids.Commands_Drop.width , self.ids.Commands_Drop.height)
+        else:
+            self.ids.Pass_Input.size = (self.ids.Commands_Drop.width , self.ids.Commands_Drop.height * 2)
+            
+
+    def infinite_loop(self):
+        iteration = 0
+        while True:
+            if self.stop.is_set():
+                # Stop running this thread so the main Python process can exit.
+                return
+            iteration += 1
+            print('Infinite loop, iteration {}.'.format(iteration))
+            time.sleep(1)
+
         
-
-    def subbuttonlabel():
-
-        print(MyLayout().ids.Submit_Button.text)
-        time.sleep(2)
-        print(MyLayout().ids.Submit_Button.text)
 buildkv = Builder.load_file("Command_UI.kv")
 
 class CommandApp(App):
+    def on_stop(self):
+        self.root.stop.set()
+
     def build(self):
         self.Commands = Commands
         return buildkv
